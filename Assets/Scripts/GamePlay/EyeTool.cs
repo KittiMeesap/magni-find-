df2c034719bde0c51775687a24c36a4f78b26c44
kitti <tkt.ms@hotmail.com>
2025-01-24 00:53:49 +0700

    Update Eye Tool 0.3

--- Assets/Scripts/GamePlay/EyeTool.cs
+++ Assets/Scripts/GamePlay/EyeTool.cs
@@ -1,26 +1,16 @@
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class EyeTool : MonoBehaviour
 {
-    public Texture2D eyeCursor;
-
-    private void Awake()
-    {
-        SetEyeCursor();
-    }
-
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (ToolManager.Instance.CurrentMode == "Eye")
         {
-            InteractWithObject();
+            if (Input.GetMouseButtonDown(0)) 
+            {
+                InteractWithObject();
+            }
         }
-    }
-
-    private void SetEyeCursor()
-    {
-        Cursor.SetCursor(eyeCursor, Vector2.zero, CursorMode.Auto);
     }
 
     private void InteractWithObject()
@@ -33,18 +23,7 @@ public class EyeTool : MonoBehaviour
             DialogueSystem dialogueSystem = hit.collider.GetComponent<DialogueSystem>();
             if (dialogueSystem != null)
             {
-                if (ToolManager.Instance.CurrentMode == "Eye")
-                {
-                    dialogueSystem.ShowDialogue();
-                }
-                else
-                {
-                    Debug.LogWarning("Use the Eye tool to interact with this item.");
-                }
-            }
-            else
-            {
-                Debug.LogWarning("No DialogueSystem attached to the object.");
+                dialogueSystem.ShowDialogue();
             }
         }
     }

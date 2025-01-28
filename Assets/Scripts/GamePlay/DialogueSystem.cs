fffbba9809cf95a77ec9dffffd2bb094278de632
kitti <tkt.ms@hotmail.com>
2025-01-28 22:45:35 +0700

    Update Dialogue System 0.5

--- Assets/Scripts/GamePlay/DialogueSystem.cs
+++ Assets/Scripts/GamePlay/DialogueSystem.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueSystem : MonoBehaviour
 {
     [TextArea]
-    public string dialogueText; 
-    public bool hasMagnifierMessage; 
-    public string dialogueTextSpecial; 
+    public string dialogueText;
+    public bool hasMagnifierMessage;
+    public string dialogueTextSpecial;
 
-    public GameObject itemObject;
-
+    public GameObject itemObject; // ?????? GameObject ????? SpriteRenderer ???? Image
 
     public void ShowDialogue()
     {
+        // ????????????? Magnifier ?????????????????
         if (ToolManager.Instance.CurrentMode == "Magnifier" && hasMagnifierMessage)
         {
             if (DialogueUI.Instance != null)
             {
+                // ??????????????????????????? itemObject
                 DialogueUI.Instance.ShowDialogue(dialogueTextSpecial, itemObject);
             }
             else
@@ -34,7 +36,5 @@ public class DialogueSystem : MonoBehaviour
                 Debug.LogError("DialogueUI instance not found in the scene.");
             }
         }
-
-
     }
 }
--- Assets/Scripts/GamePlay/DialogueUI.cs
+++ Assets/Scripts/GamePlay/DialogueUI.cs
@@ -11,6 +11,9 @@ public class DialogueUI : MonoBehaviour
     public Button closeButton;
     private GameObject currentItemObject;
 
+    // ??????????? Image ???????????????
+    public Image itemImage; // Image UI ????????????????
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -32,6 +35,11 @@ public class DialogueUI : MonoBehaviour
         {
             Debug.LogWarning("Close button not assigned in the inspector.");
         }
+
+        if (itemImage == null)
+        {
+            Debug.LogError("Item Image not assigned in the inspector.");
+        }
     }
 
     private void Update()
@@ -47,14 +55,45 @@ public class DialogueUI : MonoBehaviour
         dialogueText.text = message;
         dialoguePanel.SetActive(true);
 
+        // ???????????????????????????
         if (currentItemObject != null)
         {
             currentItemObject.SetActive(false);
         }
 
+        // ?????????? itemObject ???????????
         if (itemObject != null)
         {
+            itemObject.SetActive(true); // ???????????????? itemObject
+
             currentItemObject = itemObject;
+
+            // ?????????? itemObject ?? SpriteRenderer ???? Image ???????
+            SpriteRenderer spriteRenderer = itemObject.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                // ???????????? Image UI ??????????????????
+                itemImage.sprite = spriteRenderer.sprite;
+                itemImage.gameObject.SetActive(true); // ????????????
+            }
+            else
+            {
+                // ???????? SpriteRenderer ??????? Image
+                Image image = itemObject.GetComponent<Image>();
+                if (image != null)
+                {
+                    itemImage.sprite = image.sprite;
+                    itemImage.gameObject.SetActive(true);
+                }
+                else
+                {
+                    itemImage.gameObject.SetActive(false); // ???????????? SpriteRenderer ???? Image
+                }
+            }
+        }
+        else
+        {
+            itemImage.gameObject.SetActive(false); // ??????????????? itemObject
         }
     }
 
@@ -62,8 +101,11 @@ public class DialogueUI : MonoBehaviour
     {
         dialoguePanel.SetActive(false);
 
+        // ??????????????? itemObject
         if (currentItemObject != null)
         {
+            currentItemObject.SetActive(false); // ??????????????????????? itemObject
+
             InteractableObject interactable = currentItemObject.GetComponent<InteractableObject>();
             if (interactable != null && interactable.IsMiniGameObject)
             {
@@ -71,5 +113,7 @@ public class DialogueUI : MonoBehaviour
             }
             currentItemObject = null;
         }
+
+        itemImage.gameObject.SetActive(false); // ??????????????? dialogue
     }
 }
